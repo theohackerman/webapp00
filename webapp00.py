afa2e38b0626c2d3fcfcfde912cab9880d06c3bd
theohackerman <116382259+theohackerman@users.noreply.github.com>
2022-10-21 16:12:07 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -10,7 +10,7 @@ st.title("Bohemian rhapsody é nota 1000000!")
 st.header("queen melhor banda!")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Bem vindos!")
+st.subheader("Ruan e top mas é GAY")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")

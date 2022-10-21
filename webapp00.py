aa58bf1b497ffa63faaf3c68223a2b52d13e0ce7
theohackerman <116382259+theohackerman@users.noreply.github.com>
2022-10-21 16:07:29 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,10 +4,10 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("SENAI ZERBINI É NOTA 1000!")
+st.title("Bohemian rhapsody é nota 1000000!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Abacaxi em tudo!")
+st.header("queen melhor banda!")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Bem vindos!")
